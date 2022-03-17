453092cf759e2f99df6983245ed8a6e9a3bf6a19
CyberRor <87041881+Rordan1@users.noreply.github.com>
2022-03-16 22:40:35 -0400

    Initial commit

---
+++ if_elif.py
@@ -0,0 +1,11 @@
+print("Please answer the following question")
+
+key = input("Encrypted or Unencrypted?")
+
+if key == "Encrypted":
+    print("Welcome")
+elif key == "Unencrypted":
+    print("Not secure")
+
+else:
+    print("Invalid Response")
